232a1ffd01caa2d2a530dcd88cfe1578e59caee4
zackdarklord <zakaria.chelbi@esprit.tn>
2020-02-29 16:31:39 +0100

    fichier boundingbox

---
+++ boundingbox.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+
+
+int bounding_box ( SDL_Rect personnage ,SDL_rect entite)
+{
+int personnaga_bas=personnage.y+personnage.h;
+int personnage_haut=personnage.y;
+int personnage_droite=personnage.x+personnage.w;
+int personnage_gauche=personnage.x;
+int entite_bas=entite.y+entite.h;
+int entite_haut=entite.y;
+int entite_droite=entite.x+entite.w;
+int entite_gauche=entite.x;
+int collision;
+if(personnage_droite < entite_gauche || personnage_gauche > entite_droite || personnage_bas < entite_haut || personnage_haut > entite_bas)
+{
+collision=0;
+}
+else 
+{
+collision=1;
+}
+return collision;
+}
---
+++ boundingbox.h
@@ -0,0 +1,10 @@
+#ifndef _PROTOTYPE_JEU_H
+#define _PROTOTYPE_JEU_H
+
+SDL_rect personnage;
+SDL_Rect entite;
+
+int bounding_box ( SDL_Rect personnage ,SDL_rect entite);
+
+
+#endif
