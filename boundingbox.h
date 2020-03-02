a882aa306f2c905329deb4636e0eba115ba30d6f
zackdarklord <61380280+zackdarklord@users.noreply.github.com>
2020-03-02 23:59:52 +0400

    Update boundingbox.h

--- boundingbox.h
+++ boundingbox.h
@@ -1,7 +1,7 @@
 #ifndef _PROTOTYPE_JEU_H
 #define _PROTOTYPE_JEU_H
 
-int bounding_box ( SDL_Rect personnage ,SDL_rect entite);
+int bounding_box ( SDL_Rect personnage ,SDL_Rect entite);
 
 
 #endif
